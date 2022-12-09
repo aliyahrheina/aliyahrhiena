758df593e5a505005c16a90af5eae24890c5a850
aliyahrheina <120153860+aliyahrheina@users.noreply.github.com>
2022-12-09 12:06:28 +0700

    yUDh

--- Project UAS/Form1.cs
+++ Project UAS/Form1.cs
@@ -19,17 +19,13 @@ namespace Project_UAS
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
-<<<<<<< HEAD
-=======
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             Form2 f3 = new Form2(); 
             this.Hide();
-            f3.ShowDialog(); 
->>>>>>> 0842ec04537646eeaa3ecc3fcf38432a54b5bd8e
+            f3.ShowDialog();
         }
     }
 }
--- Project UAS/Form3.Designer.cs
+++ Project UAS/Form3.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.groupBoxStand = new System.Windows.Forms.GroupBox();
-            this.comboBoxStand = new System.Windows.Forms.ComboBox();
             this.buttonPilih = new System.Windows.Forms.Button();
+            this.comboBoxStand = new System.Windows.Forms.ComboBox();
             this.groupBoxMenu = new System.Windows.Forms.GroupBox();
-            this.comboBoxMenu = new System.Windows.Forms.ComboBox();
+            this.labelHarga = new System.Windows.Forms.Label();
             this.btnPilinMenu = new System.Windows.Forms.Button();
+            this.comboBoxMenu = new System.Windows.Forms.ComboBox();
+            this.listBoxInfo = new System.Windows.Forms.ListBox();
             this.groupBoxStand.SuspendLayout();
             this.groupBoxMenu.SuspendLayout();
             this.SuspendLayout();
@@ -44,67 +46,91 @@
             this.groupBoxStand.Controls.Add(this.comboBoxStand);
             this.groupBoxStand.Location = new System.Drawing.Point(43, 63);
             this.groupBoxStand.Name = "groupBoxStand";
-            this.groupBoxStand.Size = new System.Drawing.Size(244, 127);
+            this.groupBoxStand.Size = new System.Drawing.Size(290, 127);
             this.groupBoxStand.TabIndex = 0;
             this.groupBoxStand.TabStop = false;
             this.groupBoxStand.Text = "Stand";
             // 
-            // comboBoxStand
-            // 
-            this.comboBoxStand.FormattingEnabled = true;
-            this.comboBoxStand.Location = new System.Drawing.Point(45, 39);
-            this.comboBoxStand.Name = "comboBoxStand";
-            this.comboBoxStand.Size = new System.Drawing.Size(121, 28);
-            this.comboBoxStand.TabIndex = 0;
-            // 
             // buttonPilih
             // 
-            this.buttonPilih.Location = new System.Drawing.Point(130, 83);
+            this.buttonPilih.Location = new System.Drawing.Point(171, 83);
             this.buttonPilih.Name = "buttonPilih";
             this.buttonPilih.Size = new System.Drawing.Size(91, 38);
             this.buttonPilih.TabIndex = 1;
             this.buttonPilih.Text = "Pilih";
             this.buttonPilih.UseVisualStyleBackColor = true;
+            this.buttonPilih.Click += new System.EventHandler(this.buttonPilih_Click);
+            // 
+            // comboBoxStand
+            // 
+            this.comboBoxStand.FormattingEnabled = true;
+            this.comboBoxStand.Location = new System.Drawing.Point(19, 42);
+            this.comboBoxStand.Name = "comboBoxStand";
+            this.comboBoxStand.Size = new System.Drawing.Size(243, 28);
+            this.comboBoxStand.TabIndex = 0;
             // 
             // groupBoxMenu
             // 
+            this.groupBoxMenu.Controls.Add(this.labelHarga);
             this.groupBoxMenu.Controls.Add(this.btnPilinMenu);
             this.groupBoxMenu.Controls.Add(this.comboBoxMenu);
             this.groupBoxMenu.Location = new System.Drawing.Point(339, 63);
             this.groupBoxMenu.Name = "groupBoxMenu";
-            this.groupBoxMenu.Size = new System.Drawing.Size(243, 127);
+            this.groupBoxMenu.Size = new System.Drawing.Size(470, 136);
             this.groupBoxMenu.TabIndex = 1;
             this.groupBoxMenu.TabStop = false;
             this.groupBoxMenu.Text = "Menu";
             // 
-            // comboBoxMenu
+            // labelHarga
             // 
-            this.comboBoxMenu.FormattingEnabled = true;
-            this.comboBoxMenu.Location = new System.Drawing.Point(64, 39);
-            this.comboBoxMenu.Name = "comboBoxMenu";
-            this.comboBoxMenu.Size = new System.Drawing.Size(121, 28);
-            this.comboBoxMenu.TabIndex = 0;
+            this.labelHarga.AutoSize = true;
+            this.labelHarga.Location = new System.Drawing.Point(347, 50);
+            this.labelHarga.Name = "labelHarga";
+            this.labelHarga.Size = new System.Drawing.Size(47, 20);
+            this.labelHarga.TabIndex = 2;
+            this.labelHarga.Text = "Rp. 0";
             // 
             // btnPilinMenu
             // 
-            this.btnPilinMenu.Location = new System.Drawing.Point(126, 83);
+            this.btnPilinMenu.Location = new System.Drawing.Point(351, 89);
             this.btnPilinMenu.Name = "btnPilinMenu";
             this.btnPilinMenu.Size = new System.Drawing.Size(87, 38);
             this.btnPilinMenu.TabIndex = 1;
             this.btnPilinMenu.Text = "Pilih";
             this.btnPilinMenu.UseVisualStyleBackColor = true;
+            this.btnPilinMenu.Click += new System.EventHandler(this.btnPilinMenu_Click);
+            // 
+            // comboBoxMenu
+            // 
+            this.comboBoxMenu.FormattingEnabled = true;
+            this.comboBoxMenu.Location = new System.Drawing.Point(29, 45);
+            this.comboBoxMenu.Name = "comboBoxMenu";
+            this.comboBoxMenu.Size = new System.Drawing.Size(280, 28);
+            this.comboBoxMenu.TabIndex = 0;
+            // 
+            // listBoxInfo
+            // 
+            this.listBoxInfo.FormattingEnabled = true;
+            this.listBoxInfo.ItemHeight = 20;
+            this.listBoxInfo.Location = new System.Drawing.Point(43, 222);
+            this.listBoxInfo.Name = "listBoxInfo";
+            this.listBoxInfo.Size = new System.Drawing.Size(766, 264);
+            this.listBoxInfo.TabIndex = 2;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1205, 450);
+            this.ClientSize = new System.Drawing.Size(893, 515);
+            this.Controls.Add(this.listBoxInfo);
             this.Controls.Add(this.groupBoxMenu);
             this.Controls.Add(this.groupBoxStand);
             this.Name = "Form3";
             this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
             this.groupBoxStand.ResumeLayout(false);
             this.groupBoxMenu.ResumeLayout(false);
+            this.groupBoxMenu.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -117,5 +143,7 @@
         private System.Windows.Forms.GroupBox groupBoxMenu;
         private System.Windows.Forms.Button btnPilinMenu;
         private System.Windows.Forms.ComboBox comboBoxMenu;
+        private System.Windows.Forms.Label labelHarga;
+        private System.Windows.Forms.ListBox listBoxInfo;
     }
 }
