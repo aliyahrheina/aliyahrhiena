8389be231ddcf02dd4c8c90fd75075bd07caea5e
MarshandaP <120153912+MarshandaP@users.noreply.github.com>
2022-12-09 10:56:09 +0700

    awal

--- Project UAS/Form1.Designer.cs
+++ Project UAS/Form1.Designer.cs
@@ -29,73 +29,28 @@ namespace Project_UAS
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.comboBoxStand = new System.Windows.Forms.ComboBox();
-            this.groupBoxMenu = new System.Windows.Forms.GroupBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.groupBox1.SuspendLayout();
-            this.groupBoxMenu.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.comboBoxStand);
-            this.groupBox1.Location = new System.Drawing.Point(63, 51);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(338, 130);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "STAND";
-            // 
-            // comboBoxStand
-            // 
-            this.comboBoxStand.FormattingEnabled = true;
-            this.comboBoxStand.Location = new System.Drawing.Point(37, 57);
-            this.comboBoxStand.Name = "comboBoxStand";
-            this.comboBoxStand.Size = new System.Drawing.Size(243, 33);
-            this.comboBoxStand.TabIndex = 0;
-            // 
-            // groupBoxMenu
-            // 
-            this.groupBoxMenu.Controls.Add(this.comboBox1);
-            this.groupBoxMenu.Location = new System.Drawing.Point(496, 51);
-            this.groupBoxMenu.Name = "groupBoxMenu";
-            this.groupBoxMenu.Size = new System.Drawing.Size(378, 130);
-            this.groupBoxMenu.TabIndex = 1;
-            this.groupBoxMenu.TabStop = false;
-            this.groupBoxMenu.Text = "MENU";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(54, 57);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(262, 33);
-            this.comboBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(139, 308);
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 28.125F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 265);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 25);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Aliya";
+            this.label1.Size = new System.Drawing.Size(1560, 88);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SELAMAT DATANG DI KANTIN KLUWIH";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1224, 978);
+            this.ClientSize = new System.Drawing.Size(1602, 823);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.groupBoxMenu);
-            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "KANTIN KLUWIH";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBoxMenu.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,10 +58,6 @@ namespace Project_UAS
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.ComboBox comboBoxStand;
-        private System.Windows.Forms.GroupBox groupBoxMenu;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label1;
     }
 }
