44bf668ab294aa7648254d9928c15b3aedbd387a
MarshandaP <120153912+MarshandaP@users.noreply.github.com>
2022-12-09 10:44:47 +0700

    uas

---
+++ Project UAS/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Project_UAS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBoxStand = new System.Windows.Forms.ComboBox();
+            this.groupBoxMenu = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBoxMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBoxStand);
+            this.groupBox1.Location = new System.Drawing.Point(63, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(338, 130);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "STAND";
+            // 
+            // comboBoxStand
+            // 
+            this.comboBoxStand.FormattingEnabled = true;
+            this.comboBoxStand.Location = new System.Drawing.Point(37, 57);
+            this.comboBoxStand.Name = "comboBoxStand";
+            this.comboBoxStand.Size = new System.Drawing.Size(243, 33);
+            this.comboBoxStand.TabIndex = 0;
+            // 
+            // groupBoxMenu
+            // 
+            this.groupBoxMenu.Controls.Add(this.comboBox1);
+            this.groupBoxMenu.Location = new System.Drawing.Point(496, 51);
+            this.groupBoxMenu.Name = "groupBoxMenu";
+            this.groupBoxMenu.Size = new System.Drawing.Size(378, 130);
+            this.groupBoxMenu.TabIndex = 1;
+            this.groupBoxMenu.TabStop = false;
+            this.groupBoxMenu.Text = "MENU";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(54, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(262, 33);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1224, 978);
+            this.Controls.Add(this.groupBoxMenu);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "KANTIN KLUWIH";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBoxMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBoxStand;
+        private System.Windows.Forms.GroupBox groupBoxMenu;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Project UAS/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_UAS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxStand.Items.Add("S'Crepes");
+            comboBoxStand.Items.Add("Eat & Great");
+            comboBoxStand.Items.Add("Kakak Adek Bento");
+            comboBoxStand.Items.Add("Lima Roemah Sby");
+            comboBoxStand.Items.Add("Gado-Gado Surabaya");
+        }
+    }
+}
