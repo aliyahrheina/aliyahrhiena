f1db9e29c6831c76eeb17ccdf2f8a24b2f69fd85
aliyahrheina <120153860+aliyahrheina@users.noreply.github.com>
2022-12-20 16:23:00 +0700

    insyaallah finish

--- Project UAS/Program.cs
+++ Project UAS/Program.cs
@@ -16,7 +16,7 @@ namespace Project_UAS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
